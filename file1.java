16713b93755e1672210e07778056f13059804248
sreenathgandra12 <152074530+sreenathgandra12@users.noreply.github.com>
2024-05-16 16:43:50 +0530

    Update file1.java variable y added

--- file1.java
+++ file1.java
@@ -1,5 +1,6 @@
 public class file1 {
     int x=1000;
+    int y=1000;
     public static void main(String[] args) {
         System.out.println("first java");
         System.out.println("Second java");
