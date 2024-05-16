aef30feb46f2be9d43fdce308604d27d50ac5cd9
Sreenath <sreenathbunny1999@gmail.com>
2024-05-16 18:36:05 +0530

    s.o.p() added and sum=

--- file1.java
+++ file1.java
@@ -1,13 +1,15 @@
+
 public class file1 {
     int x=1000;
     int y=1000;
 
     public void add()
     {
-	    System.out.println(x+y);
+	    System.out.println("Sum = " +(x+y));
     }
     public static void main(String[] args) {
         System.out.println("first java");
         System.out.println("Second java");
     }
+    
 }
