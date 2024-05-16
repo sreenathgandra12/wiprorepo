121c069c60ede407b961c5621cebef08aff1ca6b
Sreenath <sreenathbunny1999@gmail.com>
2024-05-16 18:22:17 +0530

    add() added

--- file1.java
+++ file1.java
@@ -1,6 +1,11 @@
 public class file1 {
     int x=1000;
     int y=1000;
+
+    public void add()
+    {
+	    System.out.println(x+y);
+    }
     public static void main(String[] args) {
         System.out.println("first java");
         System.out.println("Second java");
