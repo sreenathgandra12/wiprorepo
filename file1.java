514e9cb19f257859c136dfa594b873e85ad56033
Sreenath <sreenathbunny1999@gmail.com>
2024-05-17 11:20:19 +0530

    update file1.java conflict

--- file1.java
+++ file1.java
@@ -1,10 +1,11 @@
+
 public class file1 {
     int x=1000;
     int y=1000;
 
     public void add()
     {
-	    System.out.println(x+y);
+	    System.out.println("Sum = " +(x+y));
     }
     public static void main(String[] args) {
         System.out.println("first java");
